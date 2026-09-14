41da2e966f8244500caa7077208c952867cd0a06
Mennz <davi.mendoza.rezende.c@gmail.com>
2026-09-14 09:44:02 -0300

    implementa bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(lista):
+    lista = lista.copy()
+    n = len(lista)
+
+    for i in range(n):
+        for j in range(n - i - 1):
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+
+    return lista
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+from bubble_sort import bubble_sort
+
 if __name__ == "__main__":
-    print("estudo de algoritmos de ordenacao")
+    numeros = [5, 2, 9, 1, 5, 6, 3]
+    print(bubble_sort(numeros))
