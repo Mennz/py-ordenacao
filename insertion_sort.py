1f7fdbc94c8806780200a290ed164f4cafb8675e
Mennz <davi.mendoza.rezende.c@gmail.com>
2026-09-14 09:44:23 -0300

    implementa insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,12 @@
+def insertion_sort(lista):
+    lista = lista.copy()
+
+    for i in range(1, len(lista)):
+        atual = lista[i]
+        j = i - 1
+        while j >= 0 and lista[j] > atual:
+            lista[j + 1] = lista[j]
+            j -= 1
+        lista[j + 1] = atual
+
+    return lista
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from bubble_sort import bubble_sort
 from selection_sort import selection_sort
+from insertion_sort import insertion_sort
 
 if __name__ == "__main__":
     numeros = [5, 2, 9, 1, 5, 6, 3]
     print(bubble_sort(numeros))
     print(selection_sort(numeros))
+    print(insertion_sort(numeros))
